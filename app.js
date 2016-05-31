512e0fc8aeb6cfca1d700aa73524c8793e0bcca4
Monkey.D.Luffy <bluecloudmatrix@gmail.com>
2016-05-31 13:57:04 +0800

    ExpressJS

--- app.js
+++ app.js
@@ -20,13 +20,20 @@ var host = (process.env.VCAP_APP_HOST || 'localhost');
 var serverStarted = false;
 
 var app = express();
+
+// 加载hbs模块
+var hbs = require('hbs');
+var imagesEngine = require('./images');
+
 app.use(express.static(__dirname + '/public'));     	// set the static files location /public/img will be /img for users
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-app.set('view engine', 'ejs');
-app.engine('.html', require('ejs').renderFile);
+app.set('view engine', 'html');
+// 运行hbs模块
+app.engine('html', hbs.__express);
+//app.engine('.html', require('ejs').renderFile);
 app.post('/order', multipartMiddleware, function(req, res) {
 	console.log(req.body);
 	res.render('order.html');
@@ -34,6 +41,11 @@ app.post('/order', multipartMiddleware, function(req, res) {
 app.get('/order.html', function(req, res) {
     //res.send('LinuxONE CaaS');
     res.render('order.html');
+});
+
+app.get('/square', function(req, res) {
+	//res.send('LinuxONE CaaS');
+	res.render('square', {entries:imagesEngine.getImagesEntries()});
 });
 
 function startServer() {
