d54e683991345acda2033de70977caa543e06229
Monkey.D.Luffy <bluecloudmatrix@gmail.com>
2016-06-13 15:09:28 +0800

    nodejs post

--- app.js
+++ app.js
@@ -5,6 +5,16 @@
 
 var express = require('express')
   , fs = require('fs');
+
+var io = require('socket.io-client');
+var mySocket = io.connect("http://9.114.15.123:10002");
+
+var globalBucket = [];
+var containerName;
+var containerImage;
+var containerPort;
+var buildDate;
+
 var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
 
 var multipart = require('connect-multiparty');
@@ -36,7 +46,15 @@ app.engine('html', hbs.__express);
 //app.engine('.html', require('ejs').renderFile);
 app.post('/order', multipartMiddleware, function(req, res) {
 	console.log(req.body);
-	res.render('order.html');
+
+	var text = 'mongodb';
+	mySocket.emit('deploy', text);
+
+	remoteInfo(res);
+
+	//res.render('order.html');
+	//res.render('order.html', {service_name:req.query.service_name, image_name:req.query.image_name,
+	//							memory_setting:req.query.memory_setting, link_to:req.query.link_to});
 });
 app.get('/order.html', function(req, res) {
     //res.send('LinuxONE CaaS');
@@ -59,6 +77,28 @@ function startServer() {
 	serverStarted = true;
 	app.listen(port);   
 	console.log("Express server listening on port " + port);
+}
+
+function remoteInfo(res)
+{
+	mySocket.on("containers", function(message) {
+		globalBucket.push(message);
+		//console.log(globalBucket);
+		/*
+		containerName = message["NAMES"];
+		containerImage = message["IMAGE"];
+		containerPort = message["PORTS"];
+		buildDate = message["CREATED"];
+
+		document.getElementById('zContainerService').innerHTML = '<li class="fh5co-include"> Service: hellomongodb</li>';
+		document.getElementById('zContainerName').innerHTML = '<li class="fh5co-include"> Name: '+containerName+'</li>';
+		document.getElementById('zContainerImage').innerHTML = '<li class="fh5co-include"> Image: '+containerImage+'</li>';
+		document.getElementById('zContainerPort').innerHTML = '<li class="fh5co-include"> Port: '+containerPort+'</li>';
+		document.getElementById('zContainerBuildDate').innerHTML = '<li class="fh5co-include"> Build Date: '+buildDate+'</li>';
+		document.getElementById('zContainerAccess').innerHTML = '<li class="fh5co-include"> you can access via: 54.223.168.107:8081</li>';
+		*/
+		res.render('order.html');
+	});
 }
 
 startServer();
